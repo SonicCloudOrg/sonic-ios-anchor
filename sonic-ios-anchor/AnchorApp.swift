cacfddb8457301cc80f37586e7fa7313202abcd8
ZhouYixun <291028775@qq.com>
2022-05-31 22:02:37 +0800

    init2

---
+++ sonic-ios-anchor/AnchorApp.swift
@@ -0,0 +1,17 @@
+//
+//  sonic_ios_anchorApp.swift
+//  sonic-ios-anchor
+//
+//  Created by Eason on 2022/5/17.
+//
+
+import SwiftUI
+
+@main
+struct sonic_ios_anchorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sonic-ios-anchor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sonic-ios-anchor
+//
+//  Created by Eason on 2022/5/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Welcome to Sonic APP")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
